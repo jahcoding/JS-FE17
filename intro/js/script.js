2c3a0a797dd47fb70f5dbec07c3d2daa788e2293
Ali Odiljonov <jahcoding@gmail.com>
2023-01-11 20:05:42 +0500

    first js lesson

---
+++ intro/js/script.js
@@ -0,0 +1,24 @@
+// console.log('log1')
+// console.log("log2")
+
+// var let const
+
+let company = "Open Web";
+let count = 12;
+
+let userImage = 'img/user-logo.png' // camelLowerCase
+let UserImage = 'img/user-logo.png' // camelUpperCase
+
+let user_image = 'img/user-logo.png' // snake_case
+
+let $someVar = 5
+
+let _ = 5
+let $ = 6
+
+// let user-image = 'img/user-logo.png' //kebab-case
+
+console.log(company)
+console.log(count)
+
+// console.log(company, count) bir qatorda chiqaradi
