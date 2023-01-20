472b1d99e747beac7fe7e41cf52861fed323cbb6
Ali Odiljonov <jahcoding@gmail.com>
2023-01-20 19:16:38 +0500

    for/while/do-while

---
+++ 5.for-while-do-while/script.js
@@ -0,0 +1,29 @@
+// console.log('Hello world')
+let fruits = ['apple', 'kiwi', 'strawberry']
+
+let i = 0
+
+//do while
+do{
+	console.log(i)
+	i++
+}
+
+while(i > 6)
+
+// while
+while(i > 6){
+	console.log(i)
+	i++
+}
+
+// for
+for(let i = 0; i < 3; ++i){
+	console.log(i + '.', fruits[i])
+}
+
+// for(let i = 0; i < 3; i++){
+// 	console.log(i + 1 + '.', fruits[i])
+// }
+
+
